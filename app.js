1ef240b7b6358f6273a8ad81ba3bccb4e28f94c7
ibraheem82 <ibraheemomikunle82@gmail.com>
2022-09-25 14:42:01 +0100

    some elements where selected.

---
+++ app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+
+
+
+
+// console.log(document.querySelector('.message'));
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number ☺';
+// console.log(document.querySelector('.message').textContent);
+
+
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent  = 40;
+
+
+
+document.querySelector('.guess').value = 50;
+console.log(document.querySelector('.message').value);
+
+document.querySelector('.guess').value = 33;
